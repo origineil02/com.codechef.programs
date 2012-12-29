9df95f9ab48043bb83696f2bb43c3b65dd6d837f
Neil <Neil@192.168.1.145>
2012-12-29 11:30:31 -0600

    Small factorial

---
+++ src/codechef/smallfactorial/Main.java
@@ -0,0 +1,37 @@
+package codechef.smallfactorial;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import static java.math.BigInteger.ONE;
+
+public class Main {
+
+  public static void main(final String[] args) throws IOException {
+//    final InputStream stream = new FileInputStream("test_small_factorial.txt");
+    final InputStream stream = System.in;
+
+    final BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+    final int linesToRead = Integer.parseInt(in.readLine());
+
+    int input = 1;
+    for (int i = 0; i < linesToRead; ++i) {
+      input = Integer.parseInt(in.readLine());
+      System.out.println(factorial(input));
+    }
+  }
+
+  public static BigInteger factorial(Integer value) {z
+
+    BigInteger result = ONE;
+    while (value != 1) {
+      result = result.multiply(BigInteger.valueOf(value));
+      --value;
+    }
+
+    return result;
+  }
+}
--- test/codechef/factorial/MainTest.java
+++ test/codechef/factorial/MainTest.java
@@ -12,48 +12,6 @@ import org.junit.Test;
 public class MainTest {
   
  
-  @Test
-  public void testMain() throws Exception {
-    String[] args = null;
-    BigInteger factorial = Main.factorial(BigInteger.valueOf(100000));
-    System.out.println(factorial.toString());
-  }
-  
-  @Test
-  public void testIteration() throws Exception {
-    String[] args = null;
-    BigInteger factorial = Main.factorialInteration(BigInteger.valueOf(500000));
-    System.out.println(factorial.toString());
-  }
-  
-  @Test
-  public void testFactorial() throws Exception {
-//    String[] args = null;
-//    BigInteger factorial = BigInteger.ONE;
-//    factorial = Main.factorial(factorial, BigInteger.valueOf(100000));
-//    System.out.println(factorial.toString());
-
-    for (int i = 15624; i < 15626; i++) {
-      System.out.print(i + " ");
-      BigInteger value = Main.factorialInteration(BigInteger.valueOf(i));
-      Main.printTrailingZeros(value);
-    }
-  }
-  
-  @Test
-  public void test()
-  {
-    final BigInteger value =BigInteger.valueOf(24);
-    
-    BigInteger factorial = Main.factorial(value.divide(BigInteger.valueOf(2)));
-    System.out.println(factorial);
-    System.out.println("--");
-    Main.printTrailingZeros(factorial);
-    
-    
-//    System.out.println(Main.factorial(BigInteger.valueOf(2).multiply(Main.factorial(BigInteger.valueOf(5)))));
-  }
-  
   /* 24-5 skip 5
    * 49-50 skip 11
    * 74-5 skip 17
