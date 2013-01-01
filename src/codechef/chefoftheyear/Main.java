3ad45158f1084fe163611995a9618482de00f3fb
Neil <Neil@192.168.1.145>
2013-01-01 16:45:07 -0600

    Chef of the Year (java) : this solution is for Jan '13 Challenge ... it works and runs w/o time exception but needs to be faster

---
+++ src/codechef/chefoftheyear/Main.java
@@ -0,0 +1,111 @@
+package codechef.chefoftheyear;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+
+    final InputStream stream = new FileInputStream("test_chefoftheyear.txt");
+    // final InputStream stream = System.in;
+    final BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+    String[] parameters = in.readLine().split(" ");
+    final Integer chefsToRead = Integer.parseInt(parameters[0]);
+    final Integer emails = Integer.parseInt(parameters[1]);
+
+    final Map<String, ChefNode> chefToData = new HashMap<String, ChefNode>();
+    final Map<String, CountryNode> countryData = new HashMap<String, CountryNode>();
+    final Map<Integer, Set<String>> votesToChefs = new HashMap<Integer, Set<String>>();
+    final Map<Integer, Set<String>> votesToCountry = new HashMap<Integer, Set<String>>();
+
+    for (int i = 0; i < chefsToRead; ++i) {
+      parameters = in.readLine().split(" ");
+      final ChefNode node = new ChefNode();
+      final CountryNode cn = countryData.containsKey(parameters[1]) ? countryData.get(parameters[1]) : new CountryNode();
+      cn.country = parameters[1];
+      node.countryNode = cn;
+      countryData.put(cn.country, cn);
+      chefToData.put(parameters[0], node);
+    }
+
+//    for (Map.Entry<String, DataNode> entry : chefToData.entrySet()) {
+//      System.out.println(entry.getKey() + " : " + entry.getValue().country );
+//    }
+
+    int maxChefVotes = 0;
+    int maxCountryVotes = 0;
+    for (int i = 0; i < emails; ++i) {
+      final String subject = in.readLine();
+      final ChefNode node = chefToData.get(subject);
+      int votes = node.votes;
+      maxChefVotes = handleChefVote(votesToChefs, votes, node, subject, maxChefVotes, countryData);
+      maxCountryVotes = handleCountryVote(chefToData.get(subject), votesToCountry, maxCountryVotes, countryData);
+    }
+    final String chef = votesToChefs.get(maxChefVotes).iterator().next();
+    final String country = votesToCountry.get(maxCountryVotes).iterator().next();
+    System.out.println(country);
+    System.out.println(chef);
+  }
+
+  private static int handleChefVote(final Map<Integer,
+          Set<String>> votesToChefs, 
+          int votes, 
+          final ChefNode node,
+          final String subject, 
+          int maxChefVotes,
+          final Map<String, CountryNode> countryData) {
+    
+    if (votesToChefs.containsKey(votes + 1)) {
+      node.votes++;
+      votesToChefs.get(node.votes).add(subject);
+    }
+    else {
+      if (votes + 1 > maxChefVotes) {
+        maxChefVotes++;
+      }
+      Set<String> chefs = new TreeSet<String>();
+      chefs.add(subject);
+      votesToChefs.put(++node.votes, chefs);
+    }
+    return maxChefVotes;
+  }
+
+  private static int handleCountryVote(ChefNode node, Map<Integer, Set<String>> votesToCountry, int maxCountryVotes,
+          final Map<String, CountryNode> countryData) {
+
+    int votes = node.countryNode.votes;
+    if (votesToCountry.containsKey(votes + 1)) {
+      node.countryNode.votes++;
+      votesToCountry.get(node.countryNode.votes).add(node.countryNode.country);
+    }
+    else {
+      if (votes + 1 > maxCountryVotes) {
+        maxCountryVotes++;
+      }
+      Set<String> countries = new TreeSet<String>();
+      countries.add(node.countryNode.country);
+      votesToCountry.put(++node.countryNode.votes, countries);
+    }
+
+    return maxCountryVotes;
+  }
+
+  private static class ChefNode {
+
+    CountryNode countryNode = new CountryNode();
+    int votes = 0;
+  }
+
+  private static class CountryNode {
+
+    String country;
+    int votes = 0;
+  }
+}
--- src/codechef/gravel/Main.java
+++ src/codechef/gravel/Main.java
@@ -4,41 +4,88 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 class Main {
 
   public static void main(String[] args) throws Exception {
 
-    //final InputStream stream = new FileInputStream("test_gravel.txt");
-      final InputStream stream = System.in;
+    final InputStream stream = new FileInputStream("test_gravel.txt");
+    // final InputStream stream = System.in;
     final BufferedReader in = new BufferedReader(new InputStreamReader(stream));
     final String[] parameters = in.readLine().split(" ");
     final Integer heaps = Integer.parseInt(parameters[0]);
     final Integer operations = Integer.parseInt(parameters[1]);
     final Integer gravel = Integer.parseInt(parameters[2]);
-    
-    final int gravelHeaps[] = new int[heaps];
-    Arrays.fill(gravelHeaps, 0, heaps-1, gravel);
-    
+
+    Map<Integer, Node> gravelHeap = new TreeMap<Integer, Node>();
+    final Node node = new Node(1, heaps, gravel);
+    for (int i = 1; i <= heaps; i++) {
+      gravelHeap.put(i, node);
+    }
+
+
     for (int i = 0; i < operations; i++) {
       String[] operation = in.readLine().split(" ");
-      
-      if("Q".equals(operation[0]))
-      {
+
+      if ("Q".equals(operation[0])) {
         final Integer index = Integer.parseInt(operation[1]);
-        System.out.println(gravelHeaps[index-1]);
+        System.out.println(gravelHeap.get(index).size);
       }
-      else
-      {
+      else {
         final Integer from = Integer.parseInt(operation[1]);
         final Integer to = Integer.parseInt(operation[2]);
         final Integer pieces = Integer.parseInt(operation[3]);
-        
-        for (int j = from-1; j <= to-1; j++) {
-          gravelHeaps[j] += pieces;
+
+        final Node blah = gravelHeap.get(from);
+
+        if (!blah.requiresSplit(from, to)) {
+          node.size += pieces;
+        }
+        else {
+          node.split(from, to, pieces);
         }
       }
+    }
+  }
+
+  private static class Node {
+
+    private Integer from;
+    private Integer to;
+    Node siblingLeft;
+    Node siblingRight;
+    Integer size;
+    Node parent;
+
+    public Node(Integer from, Integer to, Integer size) {
+      this.from = from;
+      this.to = to;
+      this.size = size;
+    }
+
+    private void split(Integer from, Integer to, Integer sizeAdjustment) {
+      if (parent == null) {
+        
+        siblingLeft = new Node(this.from, from - 1, this.size);
+        siblingLeft.parent = this;
+        this.from = from;
+        this.to = to;
+        size += sizeAdjustment;
+      }
+      else {
+        //determine affected nodes
+        if(from < this.from)
+        {
+          parent.split(from, this.from, sizeAdjustment);
+        }
+      }
+    }
+
+    private boolean requiresSplit(Integer from, Integer to) {
+      return this.from == from && this.to == to;
     }
   }
 }
