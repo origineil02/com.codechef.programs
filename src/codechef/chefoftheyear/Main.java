8b5f430d860df850e55d2b246486e4156bdc2851
Neil <Neil@192.168.1.145>
2013-01-02 00:15:46 -0600

    Chef of the Year (java) : this solution is for Jan '13 Challenge ... reworked it down to 1.65s

--- src/codechef/chefoftheyear/Main.java
+++ src/codechef/chefoftheyear/Main.java
@@ -6,106 +6,103 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
 
-    final InputStream stream = new FileInputStream("test_chefoftheyear.txt");
-    // final InputStream stream = System.in;
+    //final InputStream stream = new FileInputStream("test_chefoftheyear.txt");
+    final InputStream stream = System.in;
     final BufferedReader in = new BufferedReader(new InputStreamReader(stream));
     String[] parameters = in.readLine().split(" ");
-    final Integer chefsToRead = Integer.parseInt(parameters[0]);
-    final Integer emails = Integer.parseInt(parameters[1]);
+    final int chefsToRead = Integer.parseInt(parameters[0]);
+    final int emails = Integer.parseInt(parameters[1]);
 
-    final Map<String, ChefNode> chefToData = new HashMap<String, ChefNode>();
-    final Map<String, CountryNode> countryData = new HashMap<String, CountryNode>();
-    final Map<Integer, Set<String>> votesToChefs = new HashMap<Integer, Set<String>>();
-    final Map<Integer, Set<String>> votesToCountry = new HashMap<Integer, Set<String>>();
+    //minimum requirments
+    //able to lookup country by chef name
+    //tally countries
+    //tally chef name
+
+    if (chefsToRead == 1) {
+      parameters = in.readLine().split(" ");
+      System.out.println(parameters[1]);
+      System.out.println(parameters[0]);
+      return;
+    }
+
+    final Map<String, Node> chefToCountry = new HashMap<String, Node>();
+    final Map<String, Node> chefToChef = new HashMap<String, Node>();
+    final Map<String, Node> countryToCountry = new HashMap<String, Node>();
 
     for (int i = 0; i < chefsToRead; ++i) {
       parameters = in.readLine().split(" ");
-      final ChefNode node = new ChefNode();
-      final CountryNode cn = countryData.containsKey(parameters[1]) ? countryData.get(parameters[1]) : new CountryNode();
-      cn.country = parameters[1];
-      node.countryNode = cn;
-      countryData.put(cn.country, cn);
-      chefToData.put(parameters[0], node);
+
+      if (countryToCountry.containsKey(parameters[1])) {
+        chefToCountry.put(parameters[0], countryToCountry.get(parameters[1]));
+      }
+      else {
+        Node country = new Node(parameters[1]);
+        chefToCountry.put(parameters[0], country);
+        countryToCountry.put(parameters[1], country);
+      }
+
+      chefToChef.put(parameters[0], new Node(parameters[0]));
     }
 
-//    for (Map.Entry<String, DataNode> entry : chefToData.entrySet()) {
-//      System.out.println(entry.getKey() + " : " + entry.getValue().country );
-//    }
-
-    int maxChefVotes = 0;
-    int maxCountryVotes = 0;
+    Node chef = null;
+    Node country = null;
+    boolean firstPass = true;
     for (int i = 0; i < emails; ++i) {
       final String subject = in.readLine();
-      final ChefNode node = chefToData.get(subject);
-      int votes = node.votes;
-      maxChefVotes = handleChefVote(votesToChefs, votes, node, subject, maxChefVotes, countryData);
-      maxCountryVotes = handleCountryVote(chefToData.get(subject), votesToCountry, maxCountryVotes, countryData);
-    }
-    final String chef = votesToChefs.get(maxChefVotes).iterator().next();
-    final String country = votesToCountry.get(maxCountryVotes).iterator().next();
-    System.out.println(country);
-    System.out.println(chef);
-  }
 
-  private static int handleChefVote(final Map<Integer,
-          Set<String>> votesToChefs, 
-          int votes, 
-          final ChefNode node,
-          final String subject, 
-          int maxChefVotes,
-          final Map<String, CountryNode> countryData) {
-    
-    if (votesToChefs.containsKey(votes + 1)) {
-      node.votes++;
-      votesToChefs.get(node.votes).add(subject);
-    }
-    else {
-      if (votes + 1 > maxChefVotes) {
-        maxChefVotes++;
+      if (firstPass) {
+        chef = chefToChef.get(subject);
+        chef.votes++;
+
+        country = chefToCountry.get(subject);
+        country.votes++;
+
+        firstPass = false;
       }
-      Set<String> chefs = new TreeSet<String>();
-      chefs.add(subject);
-      votesToChefs.put(++node.votes, chefs);
-    }
-    return maxChefVotes;
-  }
+      else {
+        Node currentChef = chefToChef.get(subject);
+        currentChef.votes++;
+        chef = handleNode(currentChef, chef);
 
-  private static int handleCountryVote(ChefNode node, Map<Integer, Set<String>> votesToCountry, int maxCountryVotes,
-          final Map<String, CountryNode> countryData) {
-
-    int votes = node.countryNode.votes;
-    if (votesToCountry.containsKey(votes + 1)) {
-      node.countryNode.votes++;
-      votesToCountry.get(node.countryNode.votes).add(node.countryNode.country);
-    }
-    else {
-      if (votes + 1 > maxCountryVotes) {
-        maxCountryVotes++;
+        Node currentCountry = chefToCountry.get(subject);
+        currentCountry.votes++;
+        country = handleNode(currentCountry, country);
       }
-      Set<String> countries = new TreeSet<String>();
-      countries.add(node.countryNode.country);
-      votesToCountry.put(++node.countryNode.votes, countries);
     }
 
-    return maxCountryVotes;
+    System.out.println(country.data);
+    System.out.println(chef.data);
   }
 
-  private static class ChefNode {
-
-    CountryNode countryNode = new CountryNode();
-    int votes = 0;
+  private static Node handleNode(Node current, Node node) {
+    if (current.data.compareTo(node.data) != 0) {
+      if (current.votes > node.votes) {
+        node = current;
+      }
+      else if (current.votes == node.votes && current.data.compareTo(node.data) < 0) {
+        node = current;
+      }
+    }
+    return node;
   }
 
-  private static class CountryNode {
+  private static class Node {
 
-    String country;
+    String data;
     int votes = 0;
+
+    public Node(String data) {
+      this.data = data;
+    }
+
+    @Override
+    public String toString() {
+      return "Node{" + "data=" + data + ' ' + votes + '}';
+    }
   }
 }
